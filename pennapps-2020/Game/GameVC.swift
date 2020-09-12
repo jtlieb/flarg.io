2bdb5a94a6be35ef764974fdcbd9dedde9e3a6a0
Justin <justinxlieb@gmail.com>
2020-09-12 11:18:35 -0400

    added GameVC

---
+++ pennapps-2020/Game/GameVC.swift
@@ -0,0 +1,19 @@
+//
+//  GameVC.swift
+//  pennapps-2020
+//
+//  Created by Justin Lieb on 9/12/20.
+//  Copyright © 2020 Velleity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import ARKit
+import Firebase
+
+class GameVC: UIViewController {
+    
+    
+    @IBOutlet weak var arView: ARSCNView!
+    
+}
